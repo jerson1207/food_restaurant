54cc2cf87258482bcd0c94746d023d2395e015bc
jersonjabat <jersonjabatstudent@gmail.com>
2024-03-30 20:10:30 +0800

    update render

--- config/puma.rb
+++ config/puma.rb
@@ -31,5 +31,10 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+workers ENV.fetch("WEB_CONCURRENCY") {2}
+
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
